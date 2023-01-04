977bc248f3a38ddf2e86e9e5bfabfe256cb086ec
delClg <96474794+delClg@users.noreply.github.com>
2023-01-04 19:54:14 +0530

    Added R9C (#12)
    * Added R9C
    
    * fix: package script typo
    
    Co-authored-by: flyingdelta <flyingdelta@outlook.com>

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClient as PrismaClient2 } from "../prisma/generated/client2/index.js";
 
 const prisma = new PrismaClient();
+export const prisma2 = new PrismaClient2();
 
 export default prisma;
