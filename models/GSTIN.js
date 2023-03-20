9ee6fb9f08d55f6598859fb4b9cc1c9cd7817df5
delta68 <flyingdelta06082004@gmail.com>
2023-03-20 23:39:28 +0530

    updated the data sent from GSTINDetails, List

--- models/GSTIN.js
+++ models/GSTIN.js
@@ -2,7 +2,7 @@ import prisma from "./index.js";
 
 export const fetchGSTINList = async (scode) => {
   const data =
-    await prisma.$queryRaw`SELECT id, GSTIN, GSTINDetails, div_scode, actionRequired, review FROM DATA_1718_IIT_ALL.dbo.GSTIN_DETAILS WHERE div_scode=${scode}`;
+    await prisma.$queryRaw`SELECT id, GSTIN, GSTINDetails, div_scode, actionRequired, review, viewed FROM DATA_1718_IIT_ALL.dbo.GSTIN_DETAILS WHERE div_scode=${scode}`;
   // const data = await prisma.GSTIN_DETAILS.findMany({
   //   where: {
   //     div_scode: scode,
@@ -21,7 +21,7 @@ export const fetchGSTINList = async (scode) => {
 };
 
 export const fetchGSTINDetails = async (GSTIN) => {
-  const data = await prisma.$queryRaw`SELECT TOP 1 id, GSTIN, GSTINDetails, div_scode FROM DATA_1718_IIT_ALL.dbo.GSTIN_DETAILS WHERE GSTIN=${GSTIN}`;
+  const data = await prisma.$queryRaw`SELECT TOP 1 id, GSTIN, GSTINDetails, div_scode, actionRequired, review FROM DATA_1718_IIT_ALL.dbo.GSTIN_DETAILS WHERE GSTIN=${GSTIN}`;
 
   return data[0];
 };
