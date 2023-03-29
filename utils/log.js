f6a16d56901941cff6ac4b6add6e2d6db1244c25
Prince Singh <marvelousprince012233@gmail.com>
2023-03-29 16:57:01 +0530

    fukin hell😭🥰

--- utils/log.js
+++ utils/log.js
@@ -1,5 +1,5 @@
 const log = function () {
-  var context = `${new Date().toLocaleString()}: `;
+  var context = `${new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}: `;
   return Function.prototype.bind.call(console.log, console, context);
 }();
 
