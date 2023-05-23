74f1baf5e65a171d1c6697c7838da3767ccacdb8
karthiks373 <lit2021012@iiitl.ac.in>
2023-05-23 14:13:50 +0530

    summation logic ASMT-10

--- models/ASMT-10.js
+++ models/ASMT-10.js
@@ -1,15 +1,20 @@
 import prisma from "./index.js";
 
 export const updateASMT = async (scode, boweb, drc, further_action) => {
-  console.log("Triggering scode");
+  const previousData = await prisma.oFFICE_MASTER_ACTIONS.findUnique({
+    where: {
+      Scode: scode,
+    },
+  });
+  
   const data = await prisma.oFFICE_MASTER_ACTIONS.update({
     where: {
       Scode: scode,
     },
     data: {
-      ASMT_10_Boweb: boweb ?? 0,
-      ASMT_10_73_74: drc ?? 0,
-      ASMT_10_DRC_03: further_action ?? 0,
+      ASMT_10_Boweb: (previousData.boweb ?? 0) + boweb ?? 0,
+      ASMT_10_73_74: (previousData.drc ?? 0) + drc ?? 0,
+      ASMT_10_DRC_03: (previousData.further_action ?? 0) + further_action ?? 0,
     },
   });
 
