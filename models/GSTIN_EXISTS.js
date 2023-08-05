b46f35ced2a4f332b6c19222a578a076c95a8c5a
aadi58002 <aadi58002@gmail.com>
2023-08-05 17:12:53 +0530

    Removing the wrong function name from GSTIN_EXISTS

--- models/GSTIN_EXISTS.js
+++ models/GSTIN_EXISTS.js
@@ -1,11 +1,11 @@
 import prisma from "./index.js";
-import log from "../utils/log.js";
 
 export const fetchExistance = async (GSTIN) => {
-  const data = await prisma.GSTIN_EXISTS.findFirst({
+  const data = await prisma.GSTIN_EXISTS.findUnique({
     where: {
       GSTIN: GSTIN,
     },
   });
+  console.log(data)
   return data;
 };
